308fa59f65dfe874c85cc045b9b3ebe4c16c1d08
Elvin-HIRWA <elhirwa3@gmail.com>
2021-06-17 21:35:31 +0200

    This is comment

---
+++ ElvinApp/Addmulti.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ElvinApp
+{
+    public class Addmulti
+    { 
+        public int y(int num1, int num2,int num3)
+        {
+            int num4 = (num1 + num2) * num3;
+            Console.WriteLine(num4);
+            return 0;
+
+        }
+        
+    }
+}
---
+++ ElvinApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EgilvinApp
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter 3 numbers");
+            var a = Convert.ToInt32(Console.ReadLine());
+            var b = Convert.ToInt32(Console.ReadLine());
+            var c = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(c); 
+            Addmulti addmulti = new Addmulti();
+            addmulti.y(a, b, c);
+        }
+
+    }
+    }
+
+    
